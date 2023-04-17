cbe37b06343d3a3743e12863c9efe37623be3572
Fernando Andrade <fernandofreamunde@gmail.com>
2023-04-17 17:56:56 +0200

    style header and container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import Logo from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={Logo.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
