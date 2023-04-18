d094019c7323663737a8c5dfcafbfe2b92e13886
Fernando Andrade <fernandofreamunde@gmail.com>
2023-04-18 23:23:33 +0200

    style dummy product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,29 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
+import Image from "next/image";
+import Img1 from '../../assets/Shirt/2.png'
 
 export default function Product() {
   const { query } = useRouter()
   return (
-    <h1>Hello Product {query.id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={Img1} width={520} height={480} alt=""/>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>tshirt X</h1>
+        <span>€ 65.00</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi placeat ab similique autem ex maiores labore vel obcaecati porro veniam, enim inventore, eligendi maxime aut vero veritatis, nihil corporis suscipit!
+        </p>
+
+        <button>
+          Buy now
+        </button>
+
+      </ProductDetails>
+    </ProductContainer>
   )
 }
