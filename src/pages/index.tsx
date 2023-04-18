871914e968e71354edc3a3c6529ea934c69cd274
Fernando Andrade <fernandofreamunde@gmail.com>
2023-04-18 09:53:47 +0200

    :lipstick: style the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  span: {
-    fontWeight: 'bold'
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+// import shirt1 from '../assets/Shirt/1.png'
+import shirt2 from '../assets/Shirt/2.png'
+import shirt3 from '../assets/Shirt/3.png'
+import shirt4 from '../assets/Shirt/4.png'
+import shirt5 from '../assets/Shirt/5.png'
 
 export default function Home() {
   return (
-    <Button>confirm</Button>
+    <HomeContainer>
+      <Product >
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Tshirt 3</strong>
+          <span>30.00 €</span>
+        </footer>
+      </Product>
+      <Product >
+        <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Tshirt 2</strong>
+          <span>30.00 €</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
